80b42e46e11eb72c021fb5bf33328b19753153e0
Stetson Pierce <stetsmando@gmail.com>
2018-03-02 20:46:59 -0700

    Actually hook up the logging logic

--- index.js
+++ index.js
@@ -6,22 +6,31 @@ export default class SimpleCache {
   }) {
     this.defaultTtl = defaultTtl;
     this.namespace = namespace;
+    this.logMessages = logMessages;
   }
   get(key) {
-    console.log(`SC:Looking up cache for key:${this.namespace }${ key }`);
+    if (this.logMessages) {
+      console.log(`SC:Looking up cache for key:${this.namespace }${ key }`);
+    }
     let item = localStorage.getItem(`${ this.namespace }${ key }`);
     if (!item) {
-      console.log('SC:No item found');
+      if (this.logMessages) {
+        console.log('SC:No item found');
+      }
       return null;
     }
 
     item = JSON.parse(item);
     if (Date.now() >= item.ttl) {
-      console.log('SC:Item expired, returning null');
+      if (this.logMessages) {
+        console.log('SC:Item expired, returning null');
+      }
       return null;
     }
 
-    console.log(`SC:Cached Value\nKEY:${ this.namespace }${ key }\nVALUE:${ JSON.stringify(item, null, 2) }`);
+    if (this.logMessages) {
+      console.log(`SC:Cached Value\nKEY:${ this.namespace }${ key }\nVALUE:${ JSON.stringify(item, null, 2) }`);
+    }
     return item.value;
   }
 
@@ -33,12 +42,16 @@ export default class SimpleCache {
       expiresIn.valueOf();
 
     const item = { value, ttl };
-    console.log(`SC:Caching\nKEY: ${ this.namespace }${ key }\nVALUE: ${ JSON.stringify(item, null, 2) }`);
+    if (this.logMessages) {
+      console.log(`SC:Caching\nKEY: ${ this.namespace }${ key }\nVALUE: ${ JSON.stringify(item, null, 2) }`);
+    }
     localStorage.setItem(`${ this.namespace }${ key }`, JSON.stringify(item));
   }
 
   bust(key) {
-    console.log(`SC:Bust cache for:${ this.namespace }${ key }`);
+    if (this.logMessages) {
+      console.log(`SC:Bust cache for:${ this.namespace }${ key }`);
+    }
     localStorage.removeItem(`${ this.namespace }${ key }`);
   }
 }
