42c58f2f4a7ea199a1a38350d1f39bb9afc8e73f
Stetson Pierce <stetsmando@gmail.com>
2018-03-02 20:40:58 -0700

    Make logging optional

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ export default class SimpleCache {
   constructor({
     defaultTtl = 1000 * 60 * 60 * 24, // 24 hour default
     namespace = 'SC_',
+    logMessages = false, // No logging by default
   }) {
     this.defaultTtl = defaultTtl;
     this.namespace = namespace;
-    console.log(`Constructed with defaultTtl:${ this.defaultTtl } and namespace:${ this.namespace }`);
   }
   get(key) {
-    console.log(`Looking up cache for key:${this.namespace }${ key }`);
+    console.log(`SC:Looking up cache for key:${this.namespace }${ key }`);
     let item = localStorage.getItem(`${ this.namespace }${ key }`);
     if (!item) {
-      console.log('No item found');
+      console.log('SC:No item found');
       return null;
     }
 
     item = JSON.parse(item);
     if (Date.now() >= item.ttl) {
-      console.log('Item expired, returning null');
+      console.log('SC:Item expired, returning null');
       return null;
     }
 
-    console.log(`CACHED VALUE\nKEY:${ this.namespace }${ key }\nVALUE:${ JSON.stringify(item, null, 2) }`);
+    console.log(`SC:Cached Value\nKEY:${ this.namespace }${ key }\nVALUE:${ JSON.stringify(item, null, 2) }`);
     return item.value;
   }
 
@@ -33,12 +33,12 @@ export default class SimpleCache {
       expiresIn.valueOf();
 
     const item = { value, ttl };
-    console.log(`--CACHING--\nKEY: sc_${ key }\nVALUE: ${ JSON.stringify(item, null, 2) }`);
+    console.log(`SC:Caching\nKEY: ${ this.namespace }${ key }\nVALUE: ${ JSON.stringify(item, null, 2) }`);
     localStorage.setItem(`${ this.namespace }${ key }`, JSON.stringify(item));
   }
 
   bust(key) {
-    console.log(`Bust cache for:sc_${ key }`);
+    console.log(`SC:Bust cache for:${ this.namespace }${ key }`);
     localStorage.removeItem(`${ this.namespace }${ key }`);
   }
 }
