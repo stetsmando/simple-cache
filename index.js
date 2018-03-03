2578a0f88f83c8a32aa7311f3942753efd3c6929
Stetson Pierce <stetsmando@gmail.com>
2018-03-02 20:33:59 -0700

    Add some debugging log statements

--- index.js
+++ index.js
@@ -5,16 +5,23 @@ export default class SimpleCache {
   }) {
     this.defaultTtl = defaultTtl;
     this.namespace = namespace;
+    console.log(`Constructed with defaultTtl:${ this.defaultTtl } and namespace:${ this.namespace }`);
   }
   get(key) {
+    console.log(`Looking up cache for key:${this.namespace }${ key }`);
     let item = localStorage.getItem(`${ this.namespace }${ key }`);
-    if (!item)
+    if (!item) {
+      console.log('No item found');
       return null;
+    }
 
     item = JSON.parse(item);
-    if (Date.now() >= item.ttl)
+    if (Date.now() >= item.ttl) {
+      console.log('Item expired, returning null');
       return null;
+    }
 
+    console.log(`CACHED VALUE\nKEY:${ this.namespace }${ key }\nVALUE:${ JSON.stringify(item, null, 2) }`);
     return item.value;
   }
 
@@ -26,12 +33,12 @@ export default class SimpleCache {
       expiresIn.valueOf();
 
     const item = { value, ttl };
-    // console.log(`--CACHING--\nKEY: sc_${ key }\nVALUE: ${ JSON.stringify(item, null, 2) }`);
+    console.log(`--CACHING--\nKEY: sc_${ key }\nVALUE: ${ JSON.stringify(item, null, 2) }`);
     localStorage.setItem(`${ this.namespace }${ key }`, JSON.stringify(item));
   }
 
   bust(key) {
-    // console.log(`Bust cache for:sc_${ key }`);
+    console.log(`Bust cache for:sc_${ key }`);
     localStorage.removeItem(`${ this.namespace }${ key }`);
   }
 }
